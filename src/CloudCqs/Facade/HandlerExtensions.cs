f1ff4775d8c720415403a33914f433814549f111
Tomonori Ikeyama <ikeyama.tomonori@gmail.com>
2023-05-26 06:44:31 +0900

    Add InvokeIterate to Facade

--- src/CloudCqs/Facade/HandlerExtensions.cs
+++ src/CloudCqs/Facade/HandlerExtensions.cs
@@ -30,4 +30,39 @@ public static class HandlerExtensions
                 return result;
             }
         );
+
+    public static Handler<TResult, TResponse> InvokeIterate<
+        TResponse,
+        TParam,
+        TResult,
+        TDomainRequest,
+        TDomainResponse
+    >(
+        this Handler<TParam, TResponse> handler,
+        string description,
+        IRepository<TDomainRequest, TDomainResponse> domainClass,
+        Func<TParam, IEnumerable<TDomainRequest>> pre,
+        Func<(IEnumerable<TDomainResponse> Response, TParam Param), TResult> post
+    )
+        where TParam : notnull
+        where TResult : notnull
+        where TResponse : notnull
+        where TDomainResponse : notnull
+        where TDomainRequest : notnull =>
+        handler.Then(
+            description,
+            async (param, cancellationToken) =>
+            {
+                var requests = pre(param);
+                var responses = new List<TDomainResponse>();
+                foreach (var request in requests)
+                {
+                    var response = await domainClass.Invoke(request, cancellationToken);
+                    responses.Add(response);
+                }
+                var result = post((responses.AsEnumerable(), param));
+
+                return result;
+            }
+        );
 }
